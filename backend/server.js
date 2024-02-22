534343cc9f92899fafbd6e4e2b2f17184d535916
Enayet <enayetflweb@gamil.com>
2024-02-22 11:35:25 +0600

    commit for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,30 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDb from "./db/connectToMongoDB.js"
 import cookieParser from "cookie-parser"
+import { app, server } from "./socket/socket.js"
+import path from "path"
 
-const app = express()
 dotenv.config()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
 
 
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
-app.use("/api/user", userRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Hello Enayet')
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+
+app.get('*', (req, res) => {
+  res.send(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDb()
   console.log(`Server is running at ${PORT}`)
 })
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,13 +1,10 @@
 import { BiLogOut } from "react-icons/bi";
-// import useLogout from "../../hooks/useLogout";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-	// const { loading, logout } = useLogout();
-	const loading = false
+	const { loading, logout } = useLogout();
 
-	const logout = () => {
-		console.log('logout')
-	}
+	
 	return (
 		<div className='mt-auto'>
 			{!loading ? (
